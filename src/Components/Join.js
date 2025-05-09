4069e60bc775d1a262f12ed600784737f81dc9e8
AJ <Ayushjha949@gmail.com>
2025-05-10 00:31:09 +0530

    Update Join.js

--- src/Components/Join.js
+++ src/Components/Join.js
@@ -1,32 +1,29 @@
-import React from 'react'
+import React from 'react';
 import '../Assets/css/join.css';
 
 export const Join = () => {
   return (
     <>
-  <canvas id="particleCanvas" />
-  <div className="accent-lines">
-    <div>
-      <div />
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
-    <div>
-      <div />
-      <div />
-      <div />
-      <div />
-    </div>
-  </div>
-  <a>
-    <span className="pfp" />
-  </a>
-  <a href="#">
-    <span className="fixed">Join</span>
-  </a>
-</>
+      <canvas id="particleCanvas" />
+      <div className="accent-lines">
+        <div>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <div>
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+      <div className="pfp" /> {/* Changed from <a><span className="pfp" /></a> */}
+      <button className="fixed">Join</button> {/* Changed from <a href="#"><span className="fixed">Join</span></a> */}
+    </>
+  );
+};
 
-  )
-}
+export default Join;
